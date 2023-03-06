2aa15ef66d4806d16ee006ff4b52ef922a86409b
sayedali1 <said.ali199859@gmail.com>
2023-03-06 21:19:57 +0200

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * check_cycle - fun that check if list is cycle or not
+ * @list: list of type listint_t
+ * Return: 1 if cycle , 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *node = list;
+	listint_t *node2 = list;
+
+	if (list == NULL)
+		return (0);
+
+		for (; node != NULL && node->next != NULL;)
+		{
+			node = node->next->next;
+			node2 = node2->next;
+
+			if (node == node2)
+				return (1);
+		}
+		return (0);
+	}
+}
+
