647966117baa102dd3bc6e3a3de69ea63aede100
sayedali1 <said.ali199859@gmail.com>
2023-03-08 01:14:51 +0200

    task101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    j = 0
+    s2 = ""
+    for ch in str:
+        if j != n:
+            s2 = s2 + ch
+            
+        j += 1
+
+    return s2
