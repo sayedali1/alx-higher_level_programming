1d8e19924917ba4ffc82e88229d8f811578db8a6
sayedali1 <said.ali199859@gmail.com>
2023-03-07 16:52:20 +0200

    change_type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ else:
     lastDigit = int(str(number)[-1])
 # check the conditions
 if lastDigit == 0:
-    print(f"{lastDigit} and is zero")
+    print(f"{lastDigit:d} and is 0")
 elif lastDigit > 5:
-    print(f"{lastDigit} and is greater than 5")
+    print(f"{lastDigit:d} and is greater than 5")
 elif lastDigit < 6:
-    print(f"{lastDigit} and is less than 6 and not 0")
+    print(f"{lastDigit:d} and is less than 6 and not 0")
