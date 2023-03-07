2f549798acdda2bfea42db1f64bc9d4f37fe75dd
sayedali1 <said.ali199859@gmail.com>
2023-03-07 16:48:16 +0200

    betty

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 print("Last digit of {:d} is".format(number), end=" ")
- #check if the number is negative or not
+# check if the number is negative or not
 if number < 0:
-    lastDigit = -int(str(number)[-1]) #get the last digit
+    lastDigit = -int(str(number)[-1])   # get the last digit
 else:
     lastDigit = int(str(number)[-1])
 # check the conditions
-if lastDigit == 0: 
+if lastDigit == 0:
     print(f"{lastDigit} and is zero")
 elif lastDigit > 5:
     print(f"{lastDigit} and is greater than 5")
