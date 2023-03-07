ea42bd2d7e29d27d6e922da68b42c4e1fe4a44d8
sayedali1 <said.ali199859@gmail.com>
2023-03-07 17:37:16 +0200

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        asc_ch = ord(ch)
+        
+        if asc_ch in range(97, 123):
+            asc_ch -= 32
+        
+        print("{:c}".format(asc_ch), end="")
+    print()
