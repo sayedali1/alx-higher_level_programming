f3150873b7ee494ae5fd8ed72437deac126e93bd
sayedali1 <said.ali199859@gmail.com>
2023-03-12 02:56:22 +0200

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    bigNum = 0
+    for i in my_list:
+        if bigNum < i:
+            bigNum = i
+    return bigNum
