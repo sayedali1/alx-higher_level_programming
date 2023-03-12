f34f7311c29312be3901c1eb51aaa41a15b402ee
sayedali1 <said.ali199859@gmail.com>
2023-03-12 03:12:07 +0200

    task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/urs/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for matrix_list in matrix:  # walk through each list
+        # walk through each element in the list
+        for index, i in enumerate(matrix_list, 1):
+            # dlim between nums
+            if index == len(matrix_list):
+                space = ""
+            else:
+                space = " "
+            print("{:d}".format(i), end=space)
+        print("")
