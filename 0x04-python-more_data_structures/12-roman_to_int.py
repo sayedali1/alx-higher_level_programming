94988df62a0a94eeb2ee9051cb3dc6c148718245
sayedali1 <said.ali199859@gmail.com>
2023-03-15 15:05:46 +0200

    none condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
                   "D": 500, "M": 1000}
+    if not type(roman_string) == str or roman_string == None:
+        return 0 
     numric = 0
     last_digit = 0
     for i in roman_string[::-1]:
