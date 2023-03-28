703e17d9458debddfb5d316efc39c667f37c84a0
sayedali1 <said.ali99859@gmail.com>
2023-03-28 22:32:56 +0200

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Moudle: 2-square
+Difines class square
+"""
+
+
+class Square:
+    """
+    class defintion
+
+    Args: size of the square
+    """
+    def __init__(self, size=0):
+        """
+        initialize square
+        Attributes: __size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        calc area of the square
+
+        Return: area
+        """
+        return ((self.__size)**2)
