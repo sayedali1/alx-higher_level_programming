d512f63fffbe09e001b28d52fcbd8a9881ff1967
sayedali1 <said.ali99859@gmail.com>
2023-03-28 22:00:46 +0200

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Moudle: square
+Difines class square
+"""
+
+
+class Square:
+    """
+    class defintion
+
+    Args: size of the square
+    """
+    def __init__(self, size):
+        """
+        initialize square
+        Attributes: size of the square
+        """
+        self.__size = size
