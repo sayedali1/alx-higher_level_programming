639d5b31d75ea8006e3ea1bc967ffe72b06985c5
sayedali1 <said.ali99859@gmail.com>
2023-03-28 23:06:27 +0200

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Moudle: 2-square
+Difines class square
+"""
+
+
+class Square:
+    """
+    class defintion
+
+    Args: size of the square
+    """
+    def __init__(self, size=0):
+        """
+        initialize square
+        Attributes: __size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter
+
+        args:
+            value: set size to value
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        calc area of the square
+
+        Return: area
+        """
+        return ((self.__size)**2)
+
+    def my_print(self):
+        """  
+        print square
+
+        """
+        print("\n".join(["#" * self.size for i in range(self.size)]))
