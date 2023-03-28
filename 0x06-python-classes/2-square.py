d4c7c4cb5da24dfb667ff8da9653ef62e2ca5dcf
sayedali1 <said.ali99859@gmail.com>
2023-03-28 22:14:53 +0200

    task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Moudle: 2-square
+Difines class square
+"""
+
+
+class Square:
+    """
+    class defintion
+
+    Args: size of the square
+    """
+    def __init__(self, size=0):
+        """
+        initialize square
+        Attributes: __size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an intege")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
