c2024046ae69a48f41cf1e6de19018205eb95e37
sayedali1 <said.ali99859@gmail.com>
2023-06-12 14:05:32 +0200

    task2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
 Moudle-2: is_same class
-check exactly an instance of the specified class 
+check exactly an instance of the specified class
 """
+
+
 def is_same_class(obj, a_class):
+
     return type(obj) == a_class
