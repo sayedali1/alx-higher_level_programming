74cd1702e3e689cf77d05b8ccf69b3f59e5bb87f
sayedali1 <said.ali99859@gmail.com>
2023-06-12 14:22:13 +0200

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module-6:6-basegeometry
+contain public method
+"""
+
+
+class BaseGeometry:
+    """ contain one method """
+    def area(self):
+        """ impemented aread method """
+        raise Exception("area() is not implemented")
