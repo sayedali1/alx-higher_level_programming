f1bd4c726c942ca397385d67d87369bc816f42fa
sayedali1 <said.ali99859@gmail.com>
2023-06-13 19:43:04 +0200

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,6 @@ function that writes a string to a text file
 
 
 def append_write(filename="", text=""):
+    """ write from file """
     with open(filename, mode="w", encoding="UTF8") as f:
         return(f.write(text))
