99f9be7aa219d499aadd093afbffcba60da9ab02
sayedali1 <said.ali99859@gmail.com>
2023-06-13 20:05:16 +0200

    task10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-class student():
-
+class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    """
     def __init__(self, first_name, last_name, age):
+        """Initializes student with full name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
-    def to_json(self, attrs=None)
-        
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        """
         if attrs is None:
             return self.__dict__
         else:
             dictt = {}
             for attr in attrs:
-                if attr in self.__dict__.keys()
+                if attr in self.__dict__.keys():
                     dictt[attr] = self.__dict__[attr]
             return dictt
-
