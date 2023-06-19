42a978b8bab54dba7951264dabb0d4ccdc6c27f0
sayedali1 <said.ali99859@gmail.com>
2023-06-19 22:36:52 +0200

    taskbase

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,8 +23,8 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """ encode dic to json """
-        if list_dictionaries is None:
-            return []
+        if list_dictionaries is None or len (list_dictionaries) == 0:
+            return "[]"
 
         return json.dumps(list_dictionaries)
 
