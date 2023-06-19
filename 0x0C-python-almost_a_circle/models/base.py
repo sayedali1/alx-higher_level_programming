baf2859f2f1ce6b4d66c1b1656a16e42136ea824
sayedali1 <said.ali99859@gmail.com>
2023-06-19 22:41:54 +0200

    taskbase

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,7 @@ class Base():
     def create(cls, **dictionary):
         """ returns an instance with all attributes already set """
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
+            dummy = cls(1)
         if cls.__name__ == "Square":
             dummy = cls(1, 1)
         dummy.update(**dictionary)
