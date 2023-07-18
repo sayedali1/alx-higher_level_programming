c8c24ecee68d01dc604f0c412c7c69d4e0d773f2
sayedali1 <sayed.ali199859@gmail.com>
2023-07-18 08:12:57 -0700

    task9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates a table second_table in the database
 
-CREATE TABLE IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS second_table (
        id INT,
        name VARCHAR(256),
        score INT
