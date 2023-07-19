c0b5a71e791fc1622ed6c29e88d48c72f5fb5aee
sayedali1 <said.ali99859@gmail.com>
2023-07-19 18:07:37 +0300

    task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" 
 IDENTIFIED BY "user_0d_2_pwd";
 
+GRANT USAGE
+ON *.*
+TO 'user_0d_2'@'localhost';
+
 GRANT SELECT
 ON *.*
 TO "user_0d_2"@"localhost";
