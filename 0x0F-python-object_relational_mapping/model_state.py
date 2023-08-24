dd68675c0c899f1e823b161732cdb61c8e4b73cb
sayedali1 <said.ali99859@gmail.com>
2023-08-24 12:50:48 +0300

    task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """
-return cities that are in the state given (tables 'cities' 'states)
-parameters given to script: username, password, database, state
+Defines class State
 """
 
-import MySQLdb
-import sys
 
-if __name__ == "__main__":
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+Base = declarative_base()
 
-    cursor = db.cursor()
-    sql_cmd = """SELECT cities.name
-                 FROM states
-                 INNER JOIN cities ON states.id = cities.state_id
-                 WHERE states.name LIKE %s
-                 ORDER BY cities.id ASC"""
-    cursor.execute(sql_cmd, (sys.argv[4], ))
 
-    print(', '.join(["{:s}".format(row[0]) for row in cursor.fetchall()]))
-
-    cursor.close()
-    db.close()
+class State(Base):
+    """
+    Class State inhert from base
+    """
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
