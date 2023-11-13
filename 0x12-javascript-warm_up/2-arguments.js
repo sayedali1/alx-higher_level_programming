9eb3003656f0747b3938a4b2ddb5d7132977ebd8
saidali1 <said.ali199859@gmail.com>
2023-11-13 11:38:29 +0200

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 const process = require('node:process');
 
-if (process.argv.length > 2) {
+if (process.argv.length >= 3) {
   console.log('Argument found');
 } else {
   console.log('No argument');
