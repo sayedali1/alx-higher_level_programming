0ef4e3e1055a3b40ec098c4796fb22c9b1ec5501
saidali1 <said.ali199859@gmail.com>
2023-11-15 07:35:52 +0200

    task102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,6 +5,6 @@ const file1 = fs.readFileSync(process.argv[2], 'utf-8');
 const file2 = fs.readFileSync(process.argv[3], 'utf-8');
 
 const file3 = (file1 + '' + file2);
-fs.writeFile(process.argv[4], file3,'utf-8',err => {
+fs.writeFile(process.argv[4], file3, 'utf-8', err => {
   if (err) throw err;
 });
