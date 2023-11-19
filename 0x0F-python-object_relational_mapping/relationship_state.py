2fc476f9646137bf2099cb2c1f8920355200a526
saidali1 <said.ali199859@gmail.com>
2023-11-19 09:59:03 +0200

    task100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" create states table """
+
+from sqlalchemy import INTEGER, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import City, Base
+Base = declarative_base()
+
+
+class State(Base):
+    """ State definition """
+
+    __tablename__ = "states"
+
+    id = Column(INTEGER, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state', cascade='all, delete-orphan')
